12386d7d57cbc6d923a78c598232a669625507f8
Evgenii Egorov <egorofevgeni@gmail.com>
2022-05-28 17:57:34 +0300

    settings.py

--- PyWebTest/settings.py
+++ PyWebTest/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -19,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v+g@tlfu)%n8acw1*2jz1tg789yx@m!u8*ka65#h3@rlww*!&5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv(["ALLOWED_HOST"])]
 
 # Application definition
 

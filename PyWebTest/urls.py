83177b6a50ebebc9ea9e3d468f61f0ecdf36e82f
Evgenii Egorov <egorofevgeni@gmail.com>
2022-05-31 10:41:28 +0300

    generic templates + префикс

--- PyWebTest/urls.py
+++ PyWebTest/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('notebook.urls'))
+    path('notebook/', include('notebook.urls'))  # доп.префикс notebook для выделения приложения
 ]
--- notebook/views.py
+++ notebook/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 SERVER_VERSION = "0.0.1"
 
@@ -9,9 +10,22 @@ class IndexView(View):
         return render(request, "notebook/index.html")
 
 
-class AboutView(View):
+class AboutView(View):  # нет возможности оттестировать на ошибки
     def get(self, request):
-        context = {"server_version": SERVER_VERSION
+        template_name = "notebook/version.html"
+        context = {
+            "server_version": SERVER_VERSION
+        }
+        return render(
+            request,
+            template_name=template_name,
+            context=context)
+# Дополнительно вывести User по заданию
 
-                   }
-        return render(request, "notebook/version.html", context=context)
+class AboutTemplateView(TemplateView):  # есть возможность оттестировать на ошибки
+    template_name = "notebook/version.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["server_version"] = SERVER_VERSION
+        return context
